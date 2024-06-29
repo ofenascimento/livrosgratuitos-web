9b4e7271560c8e42eb1035ee65b431ce0dfbfff0
gomestzx <felipematheusdev@gmail.com>
2024-06-28 22:20:23 -0300

    fix: featuredBooks style

--- src/components/FeaturedBooks/FeaturedBooks.tsx
+++ src/components/FeaturedBooks/FeaturedBooks.tsx
@@ -7,7 +7,7 @@ const FeaturedBooks: React.FC = () => {
         <div className='bg-dark-background rounded-lg flex flex-wrap md:flex-nowrap'>
             <div className='w-full md:w-1/4 p-2 flex justify-center items-center relative'>
                
-                <div>
+                <div className=' pb-[100%]'>
                     <Image
                         src='/featured-books/1.webp'
                         layout='fill' 
@@ -17,12 +17,12 @@ const FeaturedBooks: React.FC = () => {
                 </div>
             </div>
             <div className='w-full md:w-3/4 flex flex-col p-2 mt-4 ml-4'>
-                <h1 className='text-3xl font-poppins' style={{
+                <h1 className=' text-2xl lg:text-3xl font-poppins' style={{
                     textDecorationColor: "#7B66FF",
                     textDecorationThickness: "5px",
                     textDecorationLine: "underline",
                 }}>Coleção Machado de Assis</h1>
-                <p className='mt-2 font-raleway'>Explorando Machado de Assis: Um mergulho nas obras icônicas do mestre da literatura brasileira. Descubra contos intrigantes como 'Dom Casmurro' e 'Memórias Póstumas de Brás Cubas', que desafiam convenções e exploram as complexidades da condição humana.</p>
+                <p className='mt-4 lg:mt-2 font-raleway'>Explorando Machado de Assis: Um mergulho nas obras icônicas do mestre da literatura brasileira. Descubra contos intrigantes como 'Dom Casmurro' e 'Memórias Póstumas de Brás Cubas', que desafiam convenções e exploram as complexidades da condição humana.</p>
                 <div className='flex gap-2 flex-wrap center py-5'>
                     <Link href='/livro?bookId=65eeabf7822f5ccbb5d70831' className=' cursor-pointer'>
                         <Image src='/featured-books/2.png' width={160} height={160} alt='' />
