9d556760474c0e2c5ace8c8a1bdac25ae861135e
gomestzx <felipematheusdev@gmail.com>
2024-10-23 01:04:00 -0300

    fix: change poesia banner

--- src/components/Skeleton/StorySliderSkeleton.tsx
+++ src/components/Skeleton/StorySliderSkeleton.tsx
@@ -5,7 +5,7 @@ const StorySliderSkeleton = () => {
   return (
     <div className="w-full overflow-x-auto py-4 scrollbar-hide lg:flex lg:justify-center lg:items-center">
       <div className="flex gap-4">
-        {[...Array(8)].map((_, index) => (
+        {[...Array(9)].map((_, index) => (
           <div key={index} className="flex-shrink-0 w-24">
             <div className="flex flex-col justify-center items-center">
               <div className="h-24 w-24 bg-gray-700 rounded-3xl animate-pulse"></div>
