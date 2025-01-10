36505234b8a460a27bb1258d72054b8724523942
gomestzx <felipematheusdev@gmail.com>
2025-01-10 09:47:09 -0300

    refactor: add background in fixed ads

--- src/components/ADS/AdBanner.tsx
+++ src/components/ADS/AdBanner.tsx
@@ -29,17 +29,17 @@ const AdBanner = ({
 
   return (
     <div
-      className={`${customClassName} w-full justify-center items-center hidden md:flex ${
-        fixed ? "fixed bottom-0 left-0 z-50 my-0" : ""
-      } `}
+      className={`${customClassName} w-full justify-center items-center hidden md:flex ${fixed ? "fixed bottom-0 left-0 z-50 my-0" : ""
+        } `}
+      style={fixed ? { backgroundColor: '#000' } : {}}
     >
       <ins
         className="adsbygoogle bg-gray-600 rounded-lg"
         style={{ display: "inline-block", width: 728, height: 90 }}
         data-ad-client="ca-pub-2529229033686497"
         data-ad-slot={dataAdSlot}
-        // data-ad-format={dataAdFormat}
-        // data-full-width-responsive={dataFullWidthResponsive?.toString()}
+      // data-ad-format={dataAdFormat}
+      // data-full-width-responsive={dataFullWidthResponsive?.toString()}
       ></ins>
     </div>
   );
