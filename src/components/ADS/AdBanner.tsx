2253c8cc75c789ed4f934c2905d2e33038c19291
gomestzx <felipematheusdev@gmail.com>
2025-01-16 20:13:30 -0300

    test: ad show

--- src/components/ADS/AdBanner.tsx
+++ src/components/ADS/AdBanner.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
 type AdBannerTypes = {
   dataAdSlot: string;
@@ -8,7 +8,7 @@ type AdBannerTypes = {
   dataFullWidthResponsive?: boolean;
   fixed?: boolean;
   customClassName?: string;
-  responsive?: boolean
+  responsive?: boolean;
 };
 
 const AdBanner = ({
@@ -19,28 +19,52 @@ const AdBanner = ({
   customClassName,
   responsive
 }: AdBannerTypes) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const insRef = useRef<HTMLModElement>(null);
+
   useEffect(() => {
     try {
-      ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push(
-        {}
-      );
+      ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push({});
     } catch (error: any) {
       console.log(error.message);
     }
+
+    const timeout = setTimeout(() => {
+      if (insRef.current && containerRef.current) {
+        // Verifica se há um <iframe> dentro do <ins>
+        const hasIframe = insRef.current.querySelector("iframe") !== null;
+        // Se houver um <iframe>, exibe o container
+        if (hasIframe) {
+          containerRef.current.style.display = "flex"; 
+          // ou remova o estilo para restaurar o padrão: containerRef.current.style.display = "";
+        }
+        // Se não houver <iframe>, mantém o container oculto
+      }
+    }, 3000); // tempo de espera ajustável
+
+    return () => clearTimeout(timeout);
   }, []);
 
   return (
     <div
-      className={`${customClassName} w-full justify-center items-center hidden md:flex ${fixed ? "fixed bottom-0 left-0 z-50 my-0" : ""
-        } `}
+      ref={containerRef}
+      style={{ display: "none" }}
+      className={`${customClassName} w-full justify-center items-center md:flex ${
+        fixed ? "fixed bottom-0 left-0 z-50 my-0" : ""
+      }`}
     >
       <ins
-        className={`adsbygoogle ${!responsive && "bg-gray-600"}  rounded-lg`}
-        style={{ display: "inline-block", width: responsive ? 'auto' : 728, height: responsive ? 'auto' : 90 }}
+        ref={insRef}
+        className={`adsbygoogle ${!responsive && "bg-gray-600"} rounded-lg`}
+        style={{
+          display: "inline-block",
+          width: responsive ? "auto" : 728,
+          height: responsive ? "auto" : 90
+        }}
         data-ad-client="ca-pub-2529229033686497"
         data-ad-slot={dataAdSlot}
-      // data-ad-format={dataAdFormat}
-      // data-full-width-responsive={dataFullWidthResponsive?.toString()}
+        // data-ad-format={dataAdFormat}
+        // data-full-width-responsive={dataFullWidthResponsive?.toString()}
       ></ins>
     </div>
   );
