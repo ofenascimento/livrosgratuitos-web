15b4ecf7aab482b75ee721060bdf35d57f127bfd
gomestzx <felipematheusdev@gmail.com>
2025-01-16 20:02:59 -0300

    test: banner without width and height

--- src/components/ADS/AdBanner.tsx
+++ src/components/ADS/AdBanner.tsx
@@ -33,11 +33,11 @@ const AdBanner = ({
     <div
       className={`${customClassName} w-full justify-center items-center hidden md:flex ${fixed ? "fixed bottom-0 left-0 z-50 my-0" : ""
         } `}
-      style={fixed ? { backgroundColor: '#000' } : {}}
     >
       <ins
         className={`adsbygoogle ${!responsive && "bg-gray-600"}  rounded-lg`}
-        style={{ display: "inline-block", width: responsive ? 'auto' : 728, height: responsive ? 'auto' : 90 }}
+        // style={{ display: "inline-block", width: responsive ? 'auto' : 728, height: responsive ? 'auto' : 90 }}
+        style={{ display: "inline-block"}}
         data-ad-client="ca-pub-2529229033686497"
         data-ad-slot={dataAdSlot}
       // data-ad-format={dataAdFormat}
