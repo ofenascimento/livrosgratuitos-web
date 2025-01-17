1f34b9ba8bda3d076d86246e62696a29eb1c4998
gomestzx <felipematheusdev@gmail.com>
2025-01-17 00:16:13 -0300

    feat: observer to show ad

--- src/components/ADS/AdBanner.tsx
+++ src/components/ADS/AdBanner.tsx
@@ -21,7 +21,7 @@ const AdBanner = ({
 }: AdBannerTypes) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const insRef = useRef<HTMLModElement>(null);
-  const [isVisible, setIsVisible] = useState(false); // Começa invisível
+  const [adUnfilled, setAdUnfilled] = useState(false);
 
   useEffect(() => {
     try {
@@ -30,18 +30,24 @@ const AdBanner = ({
       console.log(error.message);
     }
 
-    const timeout = setTimeout(() => {
-      if (insRef.current) {
-        const adStatus = insRef.current.getAttribute("data-ad-status");
-        const hasIframe = insRef.current.querySelector("iframe") !== null;
-
-        if (adStatus === "filled" && hasIframe) {
-          setIsVisible(true); // Torna visível somente se for preenchido
-        }
+    const observer = new MutationObserver(() => {
+      if (insRef.current?.getAttribute("data-ad-status") === "unfilled") {
+        setAdUnfilled(true);
+      } else {
+        setAdUnfilled(false);
       }
-    }, 100); // Verificação rápida
+    });
 
-    return () => clearTimeout(timeout);
+    if (insRef.current) {
+      observer.observe(insRef.current, {
+        attributes: true,
+        attributeFilter: ["data-ad-status"],
+      });
+    }
+
+    return () => {
+      observer.disconnect();
+    };
   }, []);
 
   return (
@@ -49,28 +55,25 @@ const AdBanner = ({
       ref={containerRef}
       className={`
         ${customClassName || ""} 
-        w-full justify-center items-center hidden md:flex 
-        transition-opacity duration-500 ease-out overflow-hidden 
+        w-full justify-center items-center hidden md:flex
+        transition-all duration-500 ease-out overflow-hidden 
         ${fixed ? "fixed bottom-0 left-0 z-50 my-0" : ""}
       `}
-      style={{
-        height: isVisible ? "auto" : 0,
-        opacity: isVisible ? 1 : 0,
-      }}
     >
-      <ins
-        ref={insRef}
-        className="adsbygoogle rounded-lg"
-        style={{
-          display: "inline-block",
-          width: responsive ? "auto" : 728,
-          height: responsive ? "auto" : 90
-        }}
-        data-ad-client="ca-pub-2529229033686497"
-        data-ad-slot={dataAdSlot}
-        // data-ad-format={dataAdFormat}
-        // data-full-width-responsive={dataFullWidthResponsive?.toString()}
-      ></ins>
+      {
+        !adUnfilled && <ins
+          ref={insRef}
+          className="adsbygoogle rounded-lg bg-slate-700 "
+          style={{
+            display: "inline-block",
+            width: responsive ? "auto" : 728,
+            height: responsive ? "auto" : 90,
+          }}
+          data-ad-client="ca-pub-2529229033686497"
+          data-ad-slot={dataAdSlot}
+        ></ins>
+      }
+
     </div>
   );
 };
