5b66cc7c84ee925d1a8bd4132889ca16a36760db
gomestzx <felipematheusdev@gmail.com>
2025-03-07 14:17:42 -0300

    fix: url api

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,3 +1,3 @@
 // export const urlApi = 'https://fluted-alloy-416520.rj.r.appspot.com';
 // export const urlApi = 'http://10.0.0.180:3000'
-export const urlApi = 'https://api.livrosgratuitos.com/'
+export const urlApi = 'https://api.livrosgratuitos.com'
