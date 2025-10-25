93e670f4c45ae2ab0a6011c6db58f5cbef35af43
gomestzx <felipematheusdev@gmail.com>
2025-10-25 17:00:35 -0300

    fix: title metatag

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -1,6 +1,6 @@
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Livros Gratuitos - Leitura Online, PDFs e Epubs de livros',
+  title: 'Livros Gratuitos - Leitura Online, PDFs e EPUBs de livros',
   description: 'O livrosgratuitos.com é uma iniciativa dedicada a democratizar a cultura, oferecendo acesso gratuito a uma vasta coleção de livros de domínio público. Nosso objetivo é levar conhecimento e literatura a todos, sem custos, disponibilizando clássicos e obras importantes de forma acessível.',
 };
