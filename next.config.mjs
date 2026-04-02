95f4aa9c4cc913642afd6c8d827560a2b54b644e
ofenascimento <felipematheusdev@gmail.com>
2026-04-02 14:51:18 -0300

    fix: next config

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,13 @@ const nextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/storage/:path*',
+        destination: 'http://147.93.67.22:9000/:path*',
+      },
+    ];
+  },
 };
-
 export default nextConfig;
