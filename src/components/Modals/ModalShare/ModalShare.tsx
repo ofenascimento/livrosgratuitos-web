0dd8a07fe113d4af526257f7f516dd95d8227ecf
ofenascimento <felipematheusdev@gmail.com>
2026-04-02 15:40:50 -0300

    fix: share modal

--- src/components/Modals/ModalShare/ModalShare.tsx
+++ src/components/Modals/ModalShare/ModalShare.tsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import { MdClose } from "react-icons/md";
-import { FaCheckDouble, FaWhatsapp, FaWhatsappSquare } from "react-icons/fa";
+import { FaCheckDouble, FaWhatsapp } from "react-icons/fa";
 import { RiFileCopyLine, RiShareBoxFill } from "react-icons/ri";
 import { BsTwitterX } from "react-icons/bs";
 import useCopyToClipboard from "@/utils/useToClipboard";
@@ -12,74 +12,70 @@ interface IModalShare {
     bookImage: string;
 }
 
-const ModalShare: React.FC<IModalShare> = ({
-    isOpen,
-    onClose,
-    bookName,
-    bookImage,
-}) => {
+const ModalShare: React.FC<IModalShare> = ({ isOpen, onClose, bookName, bookImage }) => {
     const modalRef = useRef<HTMLDivElement>(null);
     const currentUrl = encodeURIComponent(window.location.href);
-    const { isCopied, copyText } = useCopyToClipboard()
+    const { isCopied, copyText } = useCopyToClipboard();
 
     return (
-        <div
-            className={`fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 ${isOpen ? "flex" : "hidden"}`}
-        >
-            <div className="absolute inset-0" onClick={onClose}></div>
-            <div ref={modalRef} className="relative border border-slate-600 bg-[#1a202c] p-5 rounded-lg shadow-lg w-full max-w-xs md:max-w-md font-lexend">
+        <div className={`fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 ${isOpen ? "flex" : "hidden"}`}>
+            <div className="absolute inset-0" onClick={onClose} />
+            <div ref={modalRef} className="relative border border-gray-600 bg-black p-5 rounded-xl shadow-lg w-full max-w-xs md:max-w-md font-redRat">
+                
+                {/* Close */}
                 <div className="flex justify-end">
-                    <MdClose className="cursor-pointer" size={20} onClick={onClose} />
+                    <MdClose className="cursor-pointer text-white" size={20} onClick={onClose} />
                 </div>
+
+                {/* Header */}
                 <h2 className="text-3xl font-bold text-center text-main-100">
                     Compartilhar
                 </h2>
-                <p className="mt-2 text-center mb-2 text-gray-300 font-light">
+                <p className="mt-2 text-center mb-2 text-gray-400 font-normal">
                     {bookName}
                 </p>
-                <div className="w-full flex justify-center items-center mb-3">
-                    <img
-                        src={bookImage}
-                        alt=""
-                        className="w-36 h-auto"
-                    />
+
+                {/* Imagem */}
+                <div className="w-full flex justify-center items-center mb-4">
+                    <img src={bookImage} alt="" className="w-36 h-auto" />
                 </div>
+
+                {/* Copiar link */}
                 <button
-                    className="flex justify-betwee border border-slate-600 justify-between w-full items-center bg-gray-800 mb-2 p-3 rounded-md cursor-pointer"
+                    className="flex justify-between border border-gray-600 w-full items-center bg-dark-background mb-2 p-3 rounded-md cursor-pointer text-white"
                     onClick={() => copyText(window.location.href)}
                 >
                     <div className="flex items-center gap-2">
-                        <img
-                            src="/logo.png"
-                            style={{ width: "auto", height: "20px" }}
-                            alt=""
-                        />
-
-                        <p>{isCopied ? 'Link copiado' : 'Copiar link'}</p>
+                        <img src="/logo.png" style={{ width: "auto", height: "20px" }} alt="" />
+                        <p className="font-normal">{isCopied ? "Link copiado" : "Copiar link"}</p>
                     </div>
                     {isCopied ? <FaCheckDouble /> : <RiFileCopyLine size={20} />}
-
                 </button>
+
+                {/* WhatsApp */}
                 <div
-                    className="flex justify-between border border-slate-600 items-center bg-gray-800 mb-2 p-3 rounded-md cursor-pointer"
-                    onClick={() => window.open(`https://api.whatsapp.com/send?text=${currentUrl}`, '_blank')}
+                    className="flex justify-between border border-gray-600 items-center bg-dark-background mb-2 p-3 rounded-md cursor-pointer text-white"
+                    onClick={() => window.open(`https://api.whatsapp.com/send?text=${currentUrl}`, "_blank")}
                 >
                     <div className="flex items-center gap-2">
                         <FaWhatsapp size={22} />
-                        <p>Compartilhar no WhatsApp</p>
+                        <p className="font-normal">Compartilhar no WhatsApp</p>
                     </div>
                     <RiShareBoxFill />
                 </div>
+
+                {/* Twitter/X */}
                 <div
-                    className="flex justify-between border border-slate-600 items-center bg-gray-800 mb-2 p-3 rounded-md cursor-pointer"
-                    onClick={() => window.open(`https://twitter.com/intent/tweet?text=${currentUrl}`, '_blank')}
+                    className="flex justify-between border border-gray-600 items-center bg-dark-background mb-2 p-3 rounded-md cursor-pointer text-white"
+                    onClick={() => window.open(`https://twitter.com/intent/tweet?text=${currentUrl}`, "_blank")}
                 >
                     <div className="flex items-center gap-2">
                         <BsTwitterX size={20} />
-                        <p>Compartilhar no X</p>
+                        <p className="font-normal">Compartilhar no X</p>
                     </div>
                     <RiShareBoxFill />
                 </div>
+
             </div>
         </div>
     );
