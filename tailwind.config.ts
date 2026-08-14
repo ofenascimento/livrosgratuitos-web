100c0077b6c95b51f318d46bdb9334d0e30689f4
ofenascimento <felipematheusdev@gmail.com>
2026-08-14 12:06:41 -0300

    feat: add safe list in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  safelist: [
+    "bg-sepia",
+  ],
   theme: {
     extend: {
       backgroundImage: {
